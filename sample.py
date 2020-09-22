eab29126c36e737f414db4e1c36d338a9231f69a
Tucan444 <66880425+Tucan444@users.noreply.github.com>
2020-09-22 17:21:17 +0200

    Update sample.py

--- sample.py
+++ sample.py
@@ -28,6 +28,7 @@ display = pygame.Surface(surface_parameters)
 pygame.display.set_caption("Circle sky")
 clock = pygame.time.Clock()
 
+
 def sample_level(screenX):
     # !!!!!creating objects to control game
 
@@ -38,11 +39,11 @@ def sample_level(screenX):
     # !!!!!creating player
 
     # never set direction to 0
-    Player = Object("player", game.custom_id_giver, [50, 50], [0, 0], 0.01, True, [5, 5])
+    Player = Object("player", game.custom_id_giver, [50, 50], [0, 0], 0.01, True, [8, 8])
     Player.move.collisions = True  # enables collisions for player
     Player.move.speed = 5  # increasing speed so ur not super slow
     Player.move.offset = 30  # were creating 120 rays with 0.5 angle difference and we need player offset 30 angles
-    # dont try to understand the comment above its just 30 it just is
+    # don't try to understand the comment above its just 30 it just is
 
     # simulating movement so u dont start at speed 0
     Player.dir_movement = Player.move.set_start_dir_movement(Player.direction, Player.dir_movement)
@@ -58,11 +59,16 @@ def sample_level(screenX):
 
     # !!!!!creating map
 
-    game_map = load_map(map_path)
+    game_map = load_map("maps/map1")
 
     # !!!!! loading objects
 
     load_objects(game_map, 32, 32, objects, game)
+
+    # !!!!! getting the dictionary for ray_casting
+    # it contains textures for different blocks, numbers in map
+
+    ray_dictionary = get_ray_dictionary()
 
     # !!!!!game loop
 
@@ -70,6 +76,7 @@ def sample_level(screenX):
         # bg
 
         display.fill((0, 0, 0))
+        pygame.draw.rect(display, (0, 100, 0), (0, 225, 600, 225))
 
         # doing player movement
 
@@ -85,8 +92,16 @@ def sample_level(screenX):
 
         player_mid = [Player.object_pos[0] + (Player.size[0]/2), Player.object_pos[1] + (Player.size[0]/2)]
         rays.cast_rays(120, Player.direction, player_mid,
-                       game_map, Player.direction + (Player.move.offset * Player.move.degree))
+                       game_map, Player.direction + (Player.move.offset * Player.move.degree), ray_dictionary)
         # for Player_mid argument we must give middle of player
+        '''
+        for i in range(32):
+            display.blit(pygame.transform.scale(ray_dictionary["1"].slice_textures[i], (5, 40)), (100, 100))
+
+            screenX.blit(pygame.transform.scale(display, Window_size), (0, 0))
+            pygame.display.update()
+            time.sleep(0.1)
+        '''
 
         # event loop
 
@@ -99,7 +114,8 @@ def sample_level(screenX):
 
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
-                    game.alive = False
+                    pass
+                    #game.alive = False
 
                 elif event.key == K_f:
                     # remember fs = fullscreen
@@ -108,7 +124,7 @@ def sample_level(screenX):
                         screenX = pygame.display.set_mode(Window_size)
                     else:
                         screenX = pygame.display.set_mode(Window_size, pygame.FULLSCREEN)
-                
+
                 elif event.key == K_d:
                     Player.move.right = True
                 elif event.key == K_a:
@@ -140,7 +156,7 @@ def sample_level(screenX):
 
 def setup_map():
     """
-    check file : maps/map0
+    check file : maps/map1
     """ 
 
     if Path(map_path).is_file():
